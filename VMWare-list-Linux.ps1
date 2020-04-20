22b3212d70f30870c683aa79a2fe97bb248bbf79
jmpilloni <63845324+jmpilloni@users.noreply.github.com>
2020-04-20 07:41:15 +0200

    Create VMWare-list-Linux.ps1
    Creates a list of all VMWare Linux machines

---
+++ VMWare-list-Linux.ps1
@@ -0,0 +1,14 @@
+Connect-VIServer $Server 
+$VMs = Get-VM |
+Where-Object{$_.ExtensionData.Config.GuestFullname -notmatch "win"} |
+  Sort-Object -Property Name| 
+Select-Object Name,
+    @{N="Configured OS";E={$_.ExtensionData.Config.GuestFullname}},
+    @{N="IPAdress";E={ $_.guest.ipAddress[0]}},
+    @{N="Running OS";E={$_.Guest.OsFullName}}, 
+    @{N="CPU";E={$_.numCpu}},
+    @{N="Memory";E={$_.MemoryGB}},
+    @{N="VMHost";E={$_.VMHost}},
+    @{N="Powered On";E={ $_.PowerState -eq “PoweredOn”}},
+    @{n="LUN ID"; E={$_.ScsiCanonicalName}}
+$VMs | Export-Csv "C:\Temp\VMWare-Info.csv" -NoTypeInformation
